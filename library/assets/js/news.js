6f5ffe45a7f612d036200c962e10b45c6f9d6c0c
FandiAlfadhil <fandi.alfadhil.907@gmail.com>
2023-11-07 08:28:30 +0700

    my-profil

--- library/assets/js/news.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	var $newsArchiveIframe = $('.newsDetail__textWrap iframe[src*="youtube"]');
-	if($newsArchiveIframe[0]) {
-		$newsArchiveIframe.each(function(){
-			$(this).wrap('<span class="ytifWrap"></span>');
-		});
-	}
-});
